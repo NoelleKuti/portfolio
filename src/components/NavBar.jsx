66fad6a921d75dc7c895ddd4147eb67c71dff7e4
Noelle K <noeykuti@gmail.com>
2022-05-16 03:20:06 -0400

    made Link component text into separate p elements so I could then center them in the border bubble I made using CSS

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,16 +12,24 @@ const NavBar = () => {
                 <div className='mainAxis row'>
                     <img className='logo' src='/Logo.png' alt='Noelle Kuti Logo'/>
                     <Link to='/skills' className='nav-item'>
-                    Skills    
+                        <p className='nav-label'>
+                            Skills
+                        </p>    
                     </Link>
                     <Link to='/projects' className='nav-item'>
-                    Projects    
+                        <p className='nav-label'>
+                            Projects
+                        </p>  
                     </Link> 
                     <Link to='/aboutme' className='nav-item'>
-                    About Me    
+                        <p className='nav-label'>
+                            About Me
+                        </p>    
                     </Link> 
                     <Link to='/connect' className='nav-item'>
-                    Connect    
+                        <p className='nav-label'>
+                            Connect
+                        </p>    
                     </Link>
                 </div>  
             </div>
@@ -42,26 +50,35 @@ const NavBarWrapper = styled.nav`
         height: 1rem;
         align-items: center;
         justify-content: space-between;
+        margin-top: 1rem;
+        padding-right: 2rem;
     }
 
     .nav-item {
         padding: 2rem;
-        font-size: 20px;
         color: var(--eastSide);
         border: 2px solid var(--sanMarino);
-        text-align: center;
         border-radius: 50%;
-        height: 3rem;
-        width: 3rem;
+        height: 4rem;
+        width: 4rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+        
         :hover {
             color: white;
             border: 2px solid white;
         }
     }
     
+    .nav-label {
+        text-align: center;
+        width: 6rem;
+    }
     .logo {
-        height: 3rem;
-        width: 3rem;
+        height: 4rem;
+        width: 4rem;
     }
 
 `
